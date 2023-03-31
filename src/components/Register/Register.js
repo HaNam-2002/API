58974d20ea0b53cdca892fbc81fbcc4631761130
HaNam2k2 <namvuha2k@gmail.com>
2023-03-31 21:39:08 +0700

    firt commit

---
+++ src/components/Register/Register.js
@@ -0,0 +1,133 @@
+import React, { useState } from 'react';
+import "./Register.css";
+
+function Register() {
+  const [formData, setFormData] = useState({
+    user: '',
+    pass: '',
+    confirmPassword: '',
+    phone: '',
+    address: '',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:8083/accounts/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+        alert('Đăng ký thành công')
+        window.location.href = 'http://localhost:3000/login'
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        alert('Đăng ký thất bại');
+      });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        background: 'url("assets/img/login.jpg")',
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        className="formLogin"
+        onSubmit={handleSubmit}
+        style={{ border: "1px solid #ccc" }}
+      >
+        <div className="container">
+          <h1 className="title_text">ĐĂNG KÝ</h1>
+          <label htmlFor="user">
+            <b>Tên đăng nhập</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Nhập tên đăng nhập"
+            name="user"
+            value={formData.user}
+            onChange={handleChange}
+            required
+          />
+          <label htmlFor="phone">
+            <b>Số điện thoại</b>
+          </label>
+          <input
+            type="tel"
+            placeholder="Nhập số điện thoại"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            required
+          />
+          <div className="form_repass">
+            <div className="pass">
+              <label htmlFor="pass">
+                <b>Mật khẩu</b>
+              </label>
+              <input
+                type="password"
+                placeholder="Nhập mật khẩu"
+                name="pass"
+                value={formData.pass}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="re_pass">
+              <label htmlFor="confirmPassword">
+                <b>Xác nhận mật khẩu</b>
+              </label>
+              <input
+                type="password"
+                placeholder="Xác nhận mật khẩu"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <label htmlFor="address">
+            <b>Địa chỉ</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Nhập địa chỉ"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+            required
+          />
+          <div className="clearfix">
+            <button
+              type="submit"
+              style={{ marginTop: "40px", marginBottom: "40px" }}
+            >
+              Đăng ký
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
---
+++ src/components/login/Login.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { useState } from "react";
+import "./Login.css";
+import { accountService } from "../../service/account.service";
+function Login() {
+  //login
+  const [user, setuser] = useState("");
+  const [pass, setpass] = useState("");
+  const [authenticated, setauthenticated] = useState(localStorage.getItem(localStorage.getItem("authenticated")|| false));
+  const [error, setError] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    accountService.login(user, pass)
+    .then(data => {
+      if (data.user === user || data.pass === pass) {
+        alert("Tên đăng nhập hoặc mật khẩu không đúng!")
+  }
+   else {
+    setauthenticated(true)
+      localStorage.setItem("authenticated", true);
+      console.log(data);
+      localStorage.setItem("user", data);
+      window.location.href = 'http://localhost:3000'
+   }
+  })
+    ;;
+  };
+  return (
+    <div
+      style={{
+        display:'flex',
+        background: 'url("assets/img/login.jpg")',
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form className="formLogin" onSubmit={handleSubmit} method="post">
+        <div className="textcontainer">
+         <h1 className="titleText">ĐĂNG NHẬP</h1>
+        </div>
+
+        <div className="container">
+          <label for="user">
+            <b>Tên đăng nhập</b>
+          </label>
+          <input
+            id="user"
+            type="text"
+            placeholder="Nhập tên đăng nhâp"
+            name="user"
+            required
+            value={user}
+            onChange={(e) => setuser(e.target.value)}
+          />
+
+          <label for="pass">
+            <b>Mật khẩu</b>
+          </label>
+          <input
+            id="pass"
+            type="password"
+            placeholder="Nhập mật khẩu"
+            name="pass"
+            required
+            onChange={(e) => setpass(e.target.value)}
+          />
+
+          <button type="submit">Đăng nhập</button>
+          <label>
+            <input type="checkbox" checked="checked" name="remember" /> Remember
+            me
+          </label>
+        </div>
+        <div className="login_mxh">
+          <button className="fb" type="button">Facebook</button>
+          <button className="gg" type="button">Google</button>
+        </div>
+        <div className="container">
+          <span className="psw">
+            Quên <a href="quenmatkhau"> Mật khẩu?</a>
+          </span>
+          <span className="psw">
+            <a href="register">Đăng ký </a>
+          </span>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
