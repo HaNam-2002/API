b994127dd512ce0795ccc756ddeedfb367fc0cdb
ThanhMinh1602 <ntminh16201@gmail.com>
2023-04-17 23:37:18 +0700

    src/components/cart/Cart.css

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,29 +1,29 @@
 import React from "react";
+import CartItem from "./CartItem";
+import "./Cart.css";
 function Cart() {
   return (
     <>
       <div className="w3-modal-content w3-card-4">
-        <a href="/" className="far fa-arrow-alt-circle-left">
-          {"   "}Mua thêm sản phẩm khác
-        </a>
+        <div className="top-content">
+          <a href="/" className="far fa-arrow-alt-circle-left">
+            {"   "}Mua thêm sản phẩm khác
+          </a>
+        </div>
         <form className="w3-container w3-card-4" action="">
-          <p>
-            <img
-              src="https://cdn.mobilecity.vn/mobilecity-vn/images/2022/10/w300/xiaomi-redmi-note-12-xanh.png.webp"
-              alt="lỗi"
-            />
-          </p>
-          <p>
-            <label className="w3-text-blue">
-              <b>Tên danh mục</b>
-            </label>
-            <input
-              className="w3-input w3-border"
-              name="name_product"
-              type="text"
-              required
-            />
-          </p>
+          <ul className="listing-cart">
+            <CartItem index={0} />
+            <CartItem index={1} />
+          </ul>
+
+          <div className="tong_Tien">
+            <h5 className="tong_Tien_text">Tạm tính:</h5>
+            <h5 className="tong_Tien_price">25.000.000Đ</h5>
+          </div>
+          <div className="line"></div>
+          <label className="w3-text-blue">
+            <h4>Thông tin khách hàng</h4>
+          </label>
           <p>
             <button className="w3-btn w3-blue">XONG</button>
           </p>
